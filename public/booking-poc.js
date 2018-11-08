481fc8c99cc451d0c44c143f11c9fdafebc931e4
gazsi.bence <gazsi.bence@virgo.hu>
2018-11-08 14:17:42 +0100

    build process

--- public/booking-poc.js
+++ public/booking-poc.js
@@ -1,21 +1,18 @@
 if (typeof Convkit !== 'object') {
-  var Convkit = {}
+  var Convkit = {};
   Convkit.init = function () {
-    this.options = JSON.parse('__config__')
-    this.attachEventListeners()
-  }
+    this.options = JSON.parse('__config__');
+    this.attachEventListeners();
+  };
   Convkit.attachEventListeners = function () {
     document.querySelectorAll('[data-convkit-id]').forEach(function (element) {
-      element.addEventListener('click', this.addToCart.bind(this, element))
-    }.bind(this))
-  }
+      element.addEventListener('click', this.addToCart.bind(this, element));
+    }.bind(this));
+  };
   Convkit.addToCart = function () {
-    var xhr = new XMLHttpRequest()
-    xhr.open('POST', this.options.urls.addToCart, true)
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    xhr.send('product-id=' + element.getAttribute('data-convkit-id') + '&eventType=addToCart')
-  }
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', this.options.urls.addToCart, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('product-id=' + element.getAttribute('data-convkit-id') + '&eventType=addToCart');
+  };
 }
-document.querySelectorAll('[data-convkit-id]').forEach(function (element) {
-  element.addEventListener('click',)
-})
