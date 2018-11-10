c88ed18a05eb1cf3dc11530d28dad1e908516d24
gazsi.bence <gazsi.bence@virgo.hu>
2018-11-10 15:02:28 +0100

    plugin js

--- public/dist/plugin.js
+++ public/dist/plugin.js
@@ -1,15 +1,15 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global.convkit = factory());
+    (global.ConvKit = factory());
 }(this, (function () { 'use strict';
 
     function convkitCookie() {
-        this._getCookie = function (name) {
+        this.getCookie = function (name) {
             var match = document.cookie.match(RegExp('(?:^|;\\s*)' + name + '=([^;]*)'));
             return match ? match[1] : null
         };
-        this._setCookie = function (name, value) {
+        this.setCookie = function (name, value) {
             document.cookie = name + '=' + (value || '') + ' path=/';
         };
     }
@@ -25,49 +25,49 @@
         }
     }
 
-    function Convkit() {
-        this.init = function () {
-            this.options = JSON.parse(JSON.stringify(config()));
-            this.cookieHandler = new convkitCookie();
-            this.configureUserAgent();
-            this.attachEventListeners();
-            this.show();
-        };
-        this.attachEventListeners = function () {
-            document.querySelectorAll('[data-atc-convkit-id]').forEach(function (element) {
-                element.addEventListener('click', this.addToCart.bind(this));
-            }.bind(this));
-            document.querySelectorAll('[data-fav-convkit-id]').forEach(function (element) {
-                element.addEventListener('click', this.favorites.bind(this));
-            }.bind(this));
-        };
-        this.addToCart = function (e) {
-            var id = e.currentTarget.getAttribute('data-atc-convkit-id');
-            this.sendEvent(this.options.urls.addToCart, id, 'atc');
-        };
-        this.favorites = function (e) {
-            var id = e.currentTarget.getAttribute('data-fav-convkit-id');
-            this.sendEvent(this.options.urls.addToCart, id, 'fav');
-        };
-        this.show = function () {
-            var element = document.querySelector('[data-convkit-product-page]');
-            if (!element) {
-                return
-            }
-            this.sendEvent(this.options.urls.show, element.getAttribute('data-convkit-product-page'), 'show');
-        };
-        this.sendEvent = function (url, id, eventType) {
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.send('pid=' + id + '&e=' + eventType);
-        };
-        this.configureUserAgent = function () {
-            if (this.cookieHandler._getCookie('__convID')) {
-                return
-            }
-            this.cookieHandler._setCookie('__convID', 'conv' + (Math.random() * 999999999));
-        };
+    function Convkit () {
+      this.init = function () {
+        this.options = JSON.parse(JSON.stringify(config()));
+        this.cookieHandler = new convkitCookie();
+        this.configureUserAgent();
+        this.attachEventListeners();
+        this.show();
+      };
+      this.attachEventListeners = function () {
+        document.querySelectorAll('[data-atc-convkit-id]').forEach(function (element) {
+          element.addEventListener('click', this.addToCart.bind(this));
+        }.bind(this));
+        document.querySelectorAll('[data-fav-convkit-id]').forEach(function (element) {
+          element.addEventListener('click', this.favorites.bind(this));
+        }.bind(this));
+      };
+      this.addToCart = function (e) {
+        var id = e.currentTarget.getAttribute('data-atc-convkit-id');
+        this.sendEvent(this.options.urls.addToCart, id, 'atc');
+      };
+      this.favorites = function (e) {
+        var id = e.currentTarget.getAttribute('data-fav-convkit-id');
+        this.sendEvent(this.options.urls.addToCart, id, 'fav');
+      };
+      this.show = function () {
+        var element = document.querySelector('[data-convkit-product-page]');
+        if (!element) {
+          return
+        }
+        this.sendEvent(this.options.urls.show, element.getAttribute('data-convkit-product-page'), 'show');
+      };
+      this.sendEvent = function (url, id, eventType) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send('pid=' + id + '&e=' + eventType);
+      };
+      this.configureUserAgent = function () {
+        if (this.cookieHandler.getCookie('__convID')) {
+          return
+        }
+        this.cookieHandler.setCookie('__convID', 'conv' + (Math.random() * 999999999));
+      };
     }
 
     function convkitPlugin () {
